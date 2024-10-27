659e8be588bc9c9751a3ba7a17a2cdf346a52fa5
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-10-27 23:18:04 +0100

    refactor: make css_tree always available

--- crates/csslsrs/src/store.rs
+++ crates/csslsrs/src/store.rs
@@ -7,30 +7,24 @@ use crate::parser::parse_css;
 pub struct StoreEntry {
     pub document: TextDocumentItem,
     last_parsed_version: Option<i32>,
-    pub css_tree: Option<CssParse>,
+    pub css_tree: CssParse,
 }
 
 impl StoreEntry {
     pub fn update_css_tree_if_necessary(&mut self) {
-        // If the CSS tree is not yet parsed, parse it
-        if self.css_tree.is_none() {
-            self.css_tree = Some(parse_css(&self.document.text));
-            self.last_parsed_version = Some(self.document.version);
-        }
-
         // If the document has been updated, re-parse the CSS tree
-        if self.document.version != self.last_parsed_version.unwrap() {
-            self.css_tree = Some(parse_css(&self.document.text));
+        if self.last_parsed_version != Some(self.document.version) {
+            self.css_tree = parse_css(&self.document.text);
             self.last_parsed_version = Some(self.document.version);
         }
     }
 
     // default
-    pub fn new(document: TextDocumentItem) -> Self {
+    pub fn new(document: TextDocumentItem, parsed_css: CssParse) -> Self {
         Self {
             document,
             last_parsed_version: None,
-            css_tree: None,
+            css_tree: parsed_css,
         }
     }
 }
@@ -47,15 +41,15 @@ impl DocumentStore {
     }
 
     pub fn get_or_update_document(&mut self, document: TextDocumentItem) -> &StoreEntry {
+        // TODO: Figure out how to do this without cloning the document on updates
         let store_entry = self
             .documents
             .entry(document.uri.clone())
-            .and_modify(|entry| {
-                if document.version != entry.document.version {
-                    entry.document = document.clone(); // TODO: Figure out how to do this without cloning
-                }
-            })
-            .or_insert_with(|| StoreEntry::new(document));
+            .or_insert_with(|| StoreEntry::new(document.clone(), parse_css(&document.text)));
+
+        if document.version != store_entry.document.version {
+            store_entry.document = document;
+        }
 
         store_entry.update_css_tree_if_necessary();
 

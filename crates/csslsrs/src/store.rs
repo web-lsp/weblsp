4e772f5fb6233e3c5b01b5e2be24a0382f4ffeb7
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-10-31 22:32:05 +0100

    test: add tests for document store

--- crates/csslsrs/src/store.rs
+++ crates/csslsrs/src/store.rs
@@ -1,3 +1,5 @@
+use std::collections::hash_map::Entry;
+
 use biome_css_parser::CssParse;
 use lsp_types::{TextDocumentItem, Uri};
 use rustc_hash::FxHashMap;
@@ -38,27 +40,30 @@ impl DocumentStore {
     }
 
     /// Get a document from the store, updating it as well if necessary.
-    /// If the document is not in the store, it will be added.
+    /// If the document is not yet in the store, it will be added.
     pub fn get_or_update_document(&mut self, document: TextDocumentItem) -> &StoreEntry {
-        // PERF: Figure out how to do this without cloning the document on updates
-        let store_entry = self
-            .documents
-            .entry(document.uri.clone())
-            .or_insert_with(|| {
-                StoreEntry::new(
-                    document.clone(),
-                    LineIndex::new(&document.text),
-                    parse_css(&document.text),
-                )
-            });
+        let uri = document.uri.clone();
+        let store_entry = self.documents.entry(uri);
 
-        if document.version != store_entry.document.version {
-            store_entry.document = document.clone();
-            store_entry.line_index = LineIndex::new(&document.text);
-            store_entry.css_tree = parse_css(&document.text);
+        match store_entry {
+            Entry::Vacant(entry) => {
+                let line_index = LineIndex::new(&document.text);
+                let css_tree = parse_css(&document.text);
+
+                entry.insert(StoreEntry::new(document, line_index, css_tree))
+            }
+            Entry::Occupied(mut entry) => {
+                let mut_entry = entry.get_mut();
+
+                if document.version != mut_entry.document.version {
+                    mut_entry.document = document;
+                    mut_entry.line_index = LineIndex::new(&mut_entry.document.text);
+                    mut_entry.css_tree = parse_css(&mut_entry.document.text);
+                }
+
+                entry.into_mut()
+            }
         }
-
-        store_entry
     }
 
     pub fn remove(&mut self, uri: &Uri) {
@@ -69,5 +74,76 @@ impl DocumentStore {
 impl Default for DocumentStore {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    impl DocumentStore {
+        pub fn len(&self) -> usize {
+            self.documents.len()
+        }
+
+        pub fn is_empty(&self) -> bool {
+            self.documents.is_empty()
+        }
+    }
+
+    #[test]
+    fn test_document_store() {
+        let mut store = DocumentStore::new();
+
+        let document = TextDocumentItem {
+            uri: Uri::from_str("file:///test").unwrap(),
+            language_id: "css".to_string(),
+            version: 1,
+            text: "body { color: red; }".to_string(),
+        };
+
+        let store_entry = store.get_or_update_document(document.clone());
+
+        assert_eq!(store_entry.document, document);
+        assert_eq!(store.len(), 1);
+
+        // Get a new version of the same document
+        let updated_document = TextDocumentItem {
+            uri: Uri::from_str("file:///test").unwrap(),
+            language_id: "css".to_string(),
+            version: 2,
+            text: "body { color: blue; }".to_string(),
+        };
+
+        let updated_store_entry = store.get_or_update_document(updated_document.clone());
+
+        assert_eq!(updated_store_entry.document, updated_document);
+
+        // We should still only have one document in the store
+        assert_eq!(store.len(), 1);
+    }
+
+    #[test]
+    fn test_document_store_remove() {
+        let mut store = DocumentStore::new();
+
+        let document = TextDocumentItem {
+            uri: Uri::from_str("file:///test").unwrap(),
+            language_id: "css".to_string(),
+            version: 1,
+            text: "body { color: red; }".to_string(),
+        };
+
+        store.get_or_update_document(document.clone());
+
+        assert!(store.documents.contains_key(&document.uri));
+        assert!(!store.is_empty());
+
+        store.remove(&document.uri);
+
+        assert!(!store.documents.contains_key(&document.uri));
+        assert!(store.is_empty());
     }
 }

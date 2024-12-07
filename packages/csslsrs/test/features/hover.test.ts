17b1bd23f9e8f3c9554b55cf954bc3de6192e7f2
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-12-07 16:47:48 +0100

    fix: ts test

--- packages/csslsrs/test/features/hover.test.ts
+++ packages/csslsrs/test/features/hover.test.ts
@@ -1,41 +1,36 @@
-import { describe, it } from "mocha"
-import { expect } from "chai"
-import { TextDocument } from "vscode-languageserver-textdocument"
-import { get_hover } from "../../../csslsrs/dist/index"
-import cssCustomData from "../../../../crates/csslsrs/data/css-schema.json"
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import { TextDocument } from "vscode-languageserver-textdocument";
+import { get_hover } from "../../../csslsrs/dist/index";
 
 describe("Hover", () => {
-  it("Can return hover", async () => {
-    const myDocument = TextDocument.create(
-      "file:///test.css",
-      "css",
-      0,
-      `body {
+	it("Can return hover", async () => {
+		const myDocument = TextDocument.create(
+			"file:///test.css",
+			"css",
+			0,
+			`body {
   background-color: #fff;
     }`
-    )
-    const hover = await get_hover(
-      myDocument,
-      { line: 0, character: 3 },
-      cssCustomData
-    )
+		);
+		const hover = await get_hover(myDocument, { line: 0, character: 3 });
 
-    expect(hover).to.deep.equal({
-      contents: {
-        kind: "markdown",
-        value:
-          "**body**\n\n[Selector Specificity](https://developer.mozilla.org/docs/Web/CSS/Specificity): (0, 0, 1)\n\n",
-      },
-      range: {
-        start: {
-          line: 0,
-          character: 0,
-        },
-        end: {
-          line: 0,
-          character: 4,
-        },
-      },
-    })
-  })
-})
+		expect(hover).to.deep.equal({
+			contents: {
+				kind: "markdown",
+				value:
+					"**body**\n\n[Selector Specificity](https://developer.mozilla.org/docs/Web/CSS/Specificity): (0, 0, 1)\n\n",
+			},
+			range: {
+				start: {
+					line: 0,
+					character: 0,
+				},
+				end: {
+					line: 0,
+					character: 4,
+				},
+			},
+		});
+	});
+});

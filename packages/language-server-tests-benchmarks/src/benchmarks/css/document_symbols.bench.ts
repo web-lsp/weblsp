a3da0e2b1dc3c5318d5f916125c7fd15c632eff6
Goulven Clec'h <goulven.clech@protonmail.com>
2024-12-19 13:33:05 +0100

    e2e test

---
+++ packages/language-server-tests-benchmarks/src/benchmarks/css/document_symbols.bench.ts
@@ -0,0 +1,32 @@
+import { afterAll, bench, describe } from "vitest";
+import { startLanguageServer } from "../../server";
+import { fileURLToPath } from "url";
+
+const filePath = fileURLToPath(
+	new URL("../../../fixture/document_symbols_benchmark.css", import.meta.url)
+);
+
+const weblsp = await startLanguageServer(undefined, "weblsp");
+const weblspUri = (await weblsp.openTextDocument(filePath, "css")).uri;
+
+const vscodeLsp = await startLanguageServer(undefined, "vscode-css");
+const vscodeLspUri = (await vscodeLsp.openTextDocument(filePath, "css")).uri;
+
+describe("DocumentSymbols", async () => {
+	bench("weblsp - DocumentSymbols", async () => {
+		await weblsp.sendDocumentSymbolRequest(weblspUri);
+	});
+
+	if (!process.env.CODSPEED) {
+		bench("vscode-css-languageserver - DocumentSymbols", async () => {
+			await vscodeLsp.sendDocumentSymbolRequest(vscodeLspUri);
+		});
+	}
+
+	afterAll(async () => {
+		await weblsp.shutdown();
+		await vscodeLsp.shutdown();
+		await weblsp.exit();
+		await vscodeLsp.exit();
+	});
+});
